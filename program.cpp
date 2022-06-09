c2584d9addc8b8573077d142885a3dd12b893b6e
gadievrm <guvho01@gmail.com>
2022-06-09 21:15:19 +0400

    Итеративная фукция fib с выводом до n

--- program.cpp
+++ program.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int fib(int n) {
-    if (n <= 3) return 1;
-    return fib(n -  1) + fib(n - 2);
+void fib(int n)
+{
+    int a = 0;
+    int b = 1;
+    int t;
+    for (int i = 0; i < n; i++) {
+        cout << a << endl;
+        t = b;
+        b = a + b;
+        a = t;
+    }
 }
 
 int main() {
-    cout << fib(10) << endl;
+    int n = 10;
+    cout << "Pervye " << n << " chisel Fibonacci:\n";
+    fib(n);
     return 0;
 }
